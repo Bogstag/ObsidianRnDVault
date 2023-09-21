171b18dc263fcb53d96501a90c8c0649ae6e53bb
Bogstag <krister@bogstag.se>
2023-09-22 01:25:12 +0200

    let it be some js

---
+++ ObsidianRnDVault/System/scripts/classes/Plugins.js
@@ -0,0 +1,49 @@
+class Plugins {
+	listInstalledPlugins(app, navbar) {
+		// Get all plugins
+		let plugins = app.plugins.plugins;
+		let pluginKeys = Object.keys(plugins);
+		for (let pluginKey of pluginKeys) {
+			navbar.appendChild(
+				createEl("div", {
+					text: plugins[pluginKey]["manifest"]["name"],
+				})
+			);
+		}
+
+		return 0;
+	}
+
+	getPluginNames(app) {
+		let plugins = app.plugins.plugins;
+		let pluginKeys = Object.keys(plugins);
+		let pluginNames = [];
+		for (let pluginKey of pluginKeys) {
+			pluginNames[pluginKey] = plugins[pluginKey].manifest.name;
+		}
+
+		return pluginNames;
+	}
+
+	async getPluginMetadata(app, pluginId) {
+		const utilsClass = await self.require.import("System/scripts/UtilityClasses/ObjectUtils.mjs");
+		const ObjectUtils = utilsClass.default;
+		const utilsInstance = new ObjectUtils();
+
+		let plugin = app.plugins.plugins[pluginId];
+		if (!plugin) {
+			throw new Error("Plugin not found! Make sure its installed and enabled!");
+		}
+		let settings = plugin.settings;
+
+		const sortedSettings = utilsInstance.sortObjectKeys(settings);
+
+		let metadata = {
+			manifest: plugin.manifest,
+			settings: plugin.settings,
+			formatedSettings: JSON.stringify(sortedSettings, null, 2),
+		};
+
+		return metadata;
+	}
+}
